e7334db9e1fb1af5bfc4e10e991cbf378239f741
ovsij <v.ovsij@yandex.ru>
2023-10-19 23:48:36 +0300

    first commit

--- bot/database/database.py
+++ bot/database/database.py
@@ -4,10 +4,10 @@ from bot.database.models import *
 from bot.config import load_config
 config = load_config('.env')
 
-db.drop_all_tables(with_all_data=True)
+
 #db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 db.bind(provider='postgres', user=config.postgres.user, password=config.postgres.password, host='localhost', database=config.postgres.database)
-
+db.drop_all_tables(with_all_data=True)
 db.provider.converter_classes.append((Enum, EnumConverter))
 
 db.generate_mapping(create_tables=True)
