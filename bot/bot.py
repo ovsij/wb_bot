495d84fc18d2c788c5282e59872977b45a0fe7cc
ovsij <v.ovsij@yandex.ru>
2023-10-19 23:37:48 +0300

    first commit

--- bot/bot.py
+++ bot/bot.py
@@ -3,4 +3,4 @@ from aiogram import Bot
 from bot.config import Config, load_config
 
 config: Config = load_config('.env')
-bot = Bot(token=config.bot.token, parse_mode='HTML', disable_web_page_preview=True)
+bot = Bot(token=config.bot.token, parse_mode='HTML')#, disable_web_page_preview=True)
