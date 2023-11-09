d72d2b10be893b0c66cf4e833585bc14fd558d4d
ovsij <v.ovsij@yandex.ru>
2023-11-09 17:52:56 +0300

    first commit

--- bot/routers/users/messages.py
+++ bot/routers/users/messages.py
@@ -150,6 +150,7 @@ async def reports_search_keywords(message: Message, db_request: DbRequests, stat
         search_results = []
         for keyword in keywords:
             page_id = 1 if article in keyword.search_1 else 2 if article in keyword.search_2 else 3
+            print(page_id)
             page_list = keyword.search_1 if page_id == 1 else keyword.search_2 if page_id == 2 else keyword.search_3
             index = page_list.index(article) + 1
             search_results.append({keyword.keyword: [page_id, index, list(keyword.values())[0], keyword.total]})
