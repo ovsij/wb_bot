1919676ca5708203850d7038f8a083e77f6c23af
ovsij <v.ovsij@yandex.ru>
2023-11-12 16:54:06 +0300

    first commit

--- updater.py
+++ updater.py
@@ -5,11 +5,10 @@ from bot.utils import regular_payment, regular_check_test_period
 from bot.wildberries import update_sellers
 
 async def main():
-    while True:
-        task1 = asyncio.create_task(update_sellers())
-        task2 = asyncio.create_task(regular_payment())
-        task3 = asyncio.create_task(regular_check_test_period())
-        asyncio.gather(task1, task2, task3)
+    task1 = asyncio.create_task(update_sellers())
+    task2 = asyncio.create_task(regular_payment())
+    task3 = asyncio.create_task(regular_check_test_period())
+    asyncio.gather(task1, task2, task3)
 
 
 if __name__ == '__main__':
