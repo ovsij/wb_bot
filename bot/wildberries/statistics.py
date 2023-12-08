cae391c13221908b3e0cd6f1f57f71f526a74569
ovsij <v.ovsij@yandex.ru>
2023-12-08 21:01:03 +0300

    first commit

--- bot/wildberries/statistics.py
+++ bot/wildberries/statistics.py
@@ -14,6 +14,7 @@ class Statistics:
                 print(response)
                 if response.status == 200:
                     result = await response.json()
+                    print(result)
                     return result
                 else:
                     logging.info(response)
