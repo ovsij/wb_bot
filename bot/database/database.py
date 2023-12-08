a2b9027ba0a9b26ddb528c30b6e5a638414ea222
ovsij <v.ovsij@yandex.ru>
2023-12-08 23:48:48 +0300

    first commit

--- bot/database/database.py
+++ bot/database/database.py
@@ -7,10 +7,10 @@ config = load_config('.env')
 
 #db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 # локальная БД
-db.bind(provider='postgres', user=config.postgres.user, password=config.postgres.password, host='localhost', database=config.postgres.database)
+#db.bind(provider='postgres', user=config.postgres.user, password=config.postgres.password, host='localhost', database=config.postgres.database)
 
 # основная БД
-#db.bind(provider='postgres', user=config.postgres_server.user, password=config.postgres_server.password, host=config.postgres_server.host, database=config.postgres_server.database)
+db.bind(provider='postgres', user=config.postgres_server.user, password=config.postgres_server.password, host=config.postgres_server.host, database=config.postgres_server.database)
 
 #db.drop_all_tables(with_all_data=True)
 db.provider.converter_classes.append((Enum, EnumConverter))
