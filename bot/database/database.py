cacabdd0e4df30eb1ca7f2f1fc4d2097fb68e8d0
ovsij <v.ovsij@yandex.ru>
2023-12-11 19:36:10 +0300

    first commit

--- bot/database/database.py
+++ bot/database/database.py
@@ -4,13 +4,20 @@ from bot.database.models import *
 from bot.config import load_config
 config = load_config('.env')
 
+import socket
+ 
+def get_local_ip():
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    return local_ip
 
 #db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 # локальная БД
 #db.bind(provider='postgres', user=config.postgres.user, password=config.postgres.password, host='localhost', database=config.postgres.database)
 
 # основная БД
-db.bind(provider='postgres', user=config.postgres_server.user, password=config.postgres_server.password, host=config.postgres_server.host, database=config.postgres_server.database)
+host = 'localhost' if get_local_ip() == config.postgres_server.host else config.postgres_server.host
+db.bind(provider='postgres', user=config.postgres_server.user, password=config.postgres_server.password, host=host, database=config.postgres_server.database)
 
 # основная БД с сервера
 #db.bind(provider='postgres', user=config.postgres_server.user, password=config.postgres_server.password, host='localhost', database=config.postgres_server.database)
